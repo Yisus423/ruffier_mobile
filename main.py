637d2c9cb5b965982dfaf12d69dfc689d313b94f
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-05-17 20:15:44 -0400

    feat(main.py):remove the imperative components of InstrScr

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from kivy.lang import Builder
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
@@ -22,44 +25,14 @@ def check_int(str_num):
         return False
 
 
-class InstrScr(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        instr = MDLabel(text=txt_instruction)
-
-        lbl1 = MDLabel(text="Ingresa tu nombre:", halign="right")
-        self.in_name = MDTextField(multiline=False)
-        lbl2 = MDLabel(text="Ingresa tu edad:", halign="right")
-
-        self.in_age = MDTextField(text="7", multiline=False)
-        self.btn = Button(
-            text="Iniciar", size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5}
-        )
-        self.btn.on_press = self.next
-
-        line1 = BoxLayout(size_hint=(0.8, None), height="30sp")
-        line2 = BoxLayout(size_hint=(0.8, None), height="30sp")
-        line1.add_widget(lbl1)
-        line1.add_widget(self.in_name)
-        line2.add_widget(lbl2)
-        line2.add_widget(self.in_age)
-
-        outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
-        outer.add_widget(instr)
-        outer.add_widget(line1)
-        outer.add_widget(line2)
-        outer.add_widget(self.btn)
-
-        self.add_widget(outer)
-
+class InstrScr(MDScreen):
     def next(self):
         app = MDApp.get_running_app()
-        app.name = self.in_name.text
-        age = check_int(self.in_age.text)
+        app.name = self.ids.in_name.text
+        age = check_int(self.ids.in_age.text)
         if age is False or age < 7:
             app.age = 7
-            self.in_age.text = str(app.age)
+            self.ids.in_age.text = str(app.age)
         else:
             app.age = age
         self.manager.current = "pulse1"
@@ -255,13 +228,16 @@ class HeartCheck(MDApp):
     p3 = NumericProperty(0)
 
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(InstrScr(name="instr"))
-        sm.add_widget(PulseScr(name="pulse1"))
-        sm.add_widget(CheckSits(name="sits"))
-        sm.add_widget(PulseScr2(name="pulse2"))
-        sm.add_widget(Result(name="result"))
-        return sm
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Purple"
+        # sm = ScreenManager()
+        # sm.add_widget(InstrScr(name="instr"))
+        # sm.add_widget(PulseScr(name="pulse1"))
+        # sm.add_widget(CheckSits(name="sits"))
+        # sm.add_widget(PulseScr2(name="pulse2"))
+        # sm.add_widget(Result(name="result"))
+        # return sm
+        return Builder.load_file("main.kv")
 
 
 app = HeartCheck()
